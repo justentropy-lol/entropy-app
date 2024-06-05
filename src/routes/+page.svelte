5f2a5523455343e7d566e7a980cb976d2fd9b710
Uros <64430773+urosvk@users.noreply.github.com>
2024-06-05 13:16:52 -0400

    First version of site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,30 @@
-&nbsp;
+<script>
+  const blobUrl = import.meta.env.VITE_BLOB_URL
+
+  const header = blobUrl + '/assets/header-GONKLXw5hiPvtIAiTytPUIevsBLQJt.png';
+  const mine = blobUrl + '/assets/button_mine-WeIhEkBTM1t32j7KNQk7qdFueMkXhZ.png';
+	const claim = blobUrl + '/assets/button_claim-UEXufrlMCoopPp6KJowfwSI7GSaIBB.png';
+	const perfect = blobUrl + '/assets/list_perfect-6GzGJgYZHv4IPtT2e2il9IMUVqyQ8O.png';
+	const proud = blobUrl + '/assets/list_proud-Pl0lD0rbwghrIXxmhWUDy89qBYNtE8.png';
+	const tokenomics = blobUrl + '/assets/list_tokenomics-aSBfzsVfZEFIGK8WO9OfcksZLsFlLm.png';
+
+  let refHeight = 10;
+  let mtBannerMult = 1/3;
+  let mtButtonMult = 1/3;
+  let mtExplainerMult = 1.2;
+  let yExplainer = 2/3;
+</script>
+
+<img alt="header" src={header} class="w-11/12 min-w-80" style="margin-top: {refHeight*mtBannerMult}px"/>
+
+
+<div class="flex justify-around w-3/5 min-w-60" bind:clientHeight={refHeight} style="margin-top: {refHeight * mtButtonMult}px">
+  <a href="https://discord.gg/TtQAZtsru6" target="_blank" class="w-2/6 hover:opacity-75 active:opacity-50" ><img alt="mint button" src={mine}/></a>
+  <img alt="claim button" src={claim} class="w-2/6 opacity-25" />
+</div>
+
+<div class="flex-col justify-around w-7/12 min-w-72 mb-20" style="margin-top: {refHeight*mtExplainerMult}px">
+  <img alt="explainer" src={perfect} style="margin-bottom: {refHeight*yExplainer}px" />
+  <img alt="explainer" src={proud} style="margin-bottom: {refHeight*yExplainer}px" />
+  <img alt="tokenomics" src={tokenomics} />
+</div>
