6d810e188fb65374228578be54047116dfb10511
Uros <64430773+urosvk@users.noreply.github.com>
2025-03-20 14:28:51 +0100

    Updated CMC verification

--- src/routes/CMC/+page.svelte
+++ src/routes/CMC/+page.svelte
@@ -1,5 +1,13 @@
-We submitted an application to CMC on March 20, 2025. Our ticket number is # ...
-The DEXscan link is <a
-  href="https://coinmarketcap.com/dexscan/solana/FbLWxFhaETaDc6rbDnSkTfAdjwhbKxQET8ZPJ6KdMvGd/"
-  >https://coinmarketcap.com/dexscan/solana/FbLWxFhaETaDc6rbDnSkTfAdjwhbKxQET8ZPJ6KdMvGd/</a
->.
+<p>
+  We submitted an application to CMC on March 20, 2025. See this post on X: <a
+    href="https://x.com/justentropylol/status/1902713424858980766/"
+    >https://x.com/justentropylol/status/1902713424858980766/</a
+  >.
+</p>
+
+<p>
+  Our ticket number is #1089404. The DEXscan link is <a
+    href="https://coinmarketcap.com/dexscan/solana/FbLWxFhaETaDc6rbDnSkTfAdjwhbKxQET8ZPJ6KdMvGd/"
+    >https://coinmarketcap.com/dexscan/solana/FbLWxFhaETaDc6rbDnSkTfAdjwhbKxQET8ZPJ6KdMvGd/</a
+  >.
+</p>
