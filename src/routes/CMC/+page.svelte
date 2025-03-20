9adab1c57fff5d5e258564a35679ad4ab348a775
Uros <64430773+urosvk@users.noreply.github.com>
2025-03-20 14:18:00 +0100

    Added CMC verification

---
+++ src/routes/CMC/+page.svelte
@@ -0,0 +1,5 @@
+We submitted an application to CMC on March 20, 2025. Our ticket number is # ...
+The DEXscan link is <a
+  href="https://coinmarketcap.com/dexscan/solana/FbLWxFhaETaDc6rbDnSkTfAdjwhbKxQET8ZPJ6KdMvGd/"
+  >https://coinmarketcap.com/dexscan/solana/FbLWxFhaETaDc6rbDnSkTfAdjwhbKxQET8ZPJ6KdMvGd/</a
+>.
