7a9f9ce5ec6fb8de352ab34a2e344556dd94b815
Uros <64430773+urosvk@users.noreply.github.com>
2025-03-24 00:37:13 +0100

    making spl-token dependency explicit

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,10 @@ export default defineConfig({
       protocolImports: true,
     }),
   ],
+  resolve: {
+    alias: {
+      // Alias the missing module to the 'buffer' package.
+      "vite-plugin-node-polyfills/shims/buffer": "buffer",
+    },
+  },
 });
